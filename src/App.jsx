616a8cb12621f87e648bed63883b8da842a3a168
suhan8984 <suhan8984@daum.net>
2025-02-20 00:53:53 +0900

    feat: router w/ home, history page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import HomePage from "./routes/HomePage";
+import HistoryPage from "./routes/HistoryPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
-    </div>
+    <BrowserRouter>
+      <div className="template-container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/history" element={<HistoryPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
