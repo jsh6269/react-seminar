60e326597bf190cf5df574a4dd4ec2e7c6a91683
suhan8984 <suhan8984@daum.net>
2025-02-20 00:56:04 +0900

    feat: link between two

--- src/routes/HistoryPage.jsx
+++ src/routes/HistoryPage.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import CheckListIcon from "../assets/images/checklist.png";
+
 const HistoryPage = () => {
-  return <h1>HistoryPage</h1>;
+  return (
+    <>
+      <h1 className="text-start m-12 mb-8 px-1">History</h1>
+      <Link to="/">
+        <img
+          src={CheckListIcon}
+          className="absolute w-15 right-8 bottom-10 opacity-20 hover:opacity-100 transition-opacity duration-120"
+        />
+      </Link>
+    </>
+  );
 };
 
 export default HistoryPage;
--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import CalenderIcon from "../assets/images/calendar.png";
+
 const HomePage = () => {
-  return <h1>HomePage</h1>;
+  return (
+    <>
+      <h1 className="text-start m-12 mb-8 px-1">Home</h1>
+      <Link to="/history">
+        <img
+          src={CalenderIcon}
+          className="absolute w-15 right-8 bottom-10 opacity-20 hover:opacity-100 transition-opacity duration-120"
+        />
+      </Link>
+    </>
+  );
 };
 
 export default HomePage;
